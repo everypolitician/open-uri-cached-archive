e39ce0d6a0b69daf1bc38c5412d112093a2a9643
Chris Mytton <chrismytton@users.noreply.github.com>
2016-10-20 18:41:29 +0100

    Add an implementation for the README functionality
    This fills in the gem with the actual working functionality for this
    library.
    
    Most of the implementation is taken from how open-uri itself does things
    [1], which can be a bit hard to follow at times.
    
    [1]: https://github.com/ruby/ruby/blob/v2_3_1/lib/open-uri.rb#L410-L424

--- lib/open-uri-cached-archive.rb
+++ lib/open-uri-cached-archive.rb
@@ -1,5 +1,43 @@
 require 'open-uri-cached-archive/version'
+require 'yaml'
+require 'stringio'
+require 'open-uri'
 
 class OpenUriCachedArchive
-  # Your code goes here...
+  class Response
+    def initialize(meta_file)
+      @meta = YAML.load_file(meta_file)
+      @body = File.read(meta_file.gsub(/\.meta$/, ''))
+      @status = meta.delete(:status)
+      @base_uri = meta.delete(:base_uri)
+      meta.delete(:content_type)
+    end
+
+    def open_uri_response
+      StringIO.new(body).tap do |response|
+        OpenURI::Meta.init(response)
+        meta.each { |k, v| response.meta_add_field(k, v) }
+        response.status = status
+        response.base_uri = base_uri
+      end
+    end
+
+    private
+
+    attr_reader :meta, :body, :status, :base_uri
+  end
+
+  def initialize(path)
+    @path = path
+  end
+
+  def responses
+    Dir.glob(File.join(path, '**', '*.meta')).map do |meta_file|
+      Response.new(meta_file).open_uri_response
+    end
+  end
+
+  private
+
+  attr_reader :path
 end
